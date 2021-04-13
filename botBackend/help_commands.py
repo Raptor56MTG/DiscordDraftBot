ff4998ef77fb67b6808ac6380045ab263eb42151
Raptor56 <82344270+Raptor56MTG@users.noreply.github.com>
2021-04-13 01:56:24 -0500

    reformatted help command file
    cleaned up the code

--- botBackend/help_commands.py
+++ botBackend/help_commands.py
@@ -2,19 +2,18 @@ def help_draft(command : str) -> str:
         
     """displays how to use each command for the bot."""
 
-    fileName = (command + ".txt").lower()
+    fileName = command.lower() + ".txt"
 
     try: 
         help_text = open("helpCommands/" + fileName, "r")
     
-    except FileNotFoundError as e:
-        return "No command found."
-    
-    else:
         help_message = ""
         for line in help_text:
             help_message += line
 
         help_text.close()
         return help_message
-        
+
+    except FileNotFoundError as e:
+        return "No command found."
+            
